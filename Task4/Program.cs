ed19b37c02322c7d1d512bad55d4cf44227da18a
Julia <ulia.0496@gmail.com>
2023-01-16 10:02:10 +0300

    done task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4
+Console.Clear();
+Console.Write("Введите 1-ое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ое число:");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+   Console.WriteLine(a);
+else if(b > c)
+   Console.WriteLine(b);
+else if (c > a)
+   Console.WriteLine(c);
+else
+   Console.WriteLine("=");
