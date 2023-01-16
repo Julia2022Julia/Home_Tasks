73062f6204f4085bd6e0eb61261f99594025fb9e
Julia <ulia.0496@gmail.com>
2023-01-16 10:34:54 +0300

    done task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8
+Console.Clear();
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a - (a - 1);
+while(b <= a )
+{
+   if(b % 2 == 0)
+      Console.Write($"{b}");
+      b++;
+}
