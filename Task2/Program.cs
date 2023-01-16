1eaf5796777033d9739990d3915840dd3b79bcd8
Julia <ulia.0496@gmail.com>
2023-01-16 09:54:20 +0300

    done task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2
+Console.Clear();
+Console.Write("Введите 1-ое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+   Console.WriteLine($"min = {b}, max = {a}");
+else if(b > a)
+   Console.WriteLine($"min = {a}, max = {b}");
+else
+   Console.WriteLine($"{a} = {b}");
