572f07f30d9eade4e8107366883248d760e62e74
Julia <ulia.0496@gmail.com>
2023-01-18 09:12:33 +0300

    comment

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,11 +6,24 @@ Console.Write("Введите 2-ое число:");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 3-ое число:");
 int c = Convert.ToInt32(Console.ReadLine());
-if(a > b)
-   Console.WriteLine(a);
-else if(b > c)
-   Console.WriteLine(b);
+if (a > b)
+    Console.WriteLine(a);
+else if (b > c)
+    Console.WriteLine(b);
 else if (c > a)
-   Console.WriteLine(c);
+    Console.WriteLine(c);
 else
-   Console.WriteLine("=");
+    Console.WriteLine("=");
+
+//Вариант от преподователя
+// Console.Write("Введите 1-ое число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 3-ое число:");
+// int c = Convert.ToInt32(Console.ReadLine()), max = a;
+
+// if (max < b) max = b;
+// if (max < c) max = c;
+
+// Console.WriteLine(max);
