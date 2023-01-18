b4544f5d02cd8e58c6933673eae8b75387ad6b4f
Julia <ulia.0496@gmail.com>
2023-01-18 19:24:59 +0300

    done task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа выводит третью цифру заданного числа
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());   //не уверенна насчет Int32
+
+if(n>1000)
+{
+         while(n > 1000)
+         {
+            n = n / 10;
+         }
+    Console.Write(n = n % 10);
+}
+   
+else if (n > 99 && n < 1000)
+        Console.Write(n = n % 10);
+         
+     
+else
+Console.Write("Цифры нет");
+
+
