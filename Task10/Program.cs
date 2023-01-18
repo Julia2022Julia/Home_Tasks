e466e9e1f5da992c7695a3b5a599db02e1408ec4
Julia <ulia.0496@gmail.com>
2023-01-18 19:13:59 +0300

    done task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Программа принимает 3-ехзначное число, на выходе - 2-ая цифра
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine()), m;
+Console.WriteLine($"{m = n / 10 % 10}");
