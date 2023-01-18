344faa9997ec80dc824a0cfbd24d37be73f7a97b
Julia <ulia.0496@gmail.com>
2023-01-18 09:24:17 +0300

    comment

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -2,7 +2,7 @@
 Console.Clear();
 Console.Write("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
-if(a % 2 == 0)
+if(a % 2 == 0)  // (a % 2 == 1) - нечетное
    Console.WriteLine($"Число '{a}' чётное");
 else
    Console.WriteLine($"Число '{a}' нечётное");
