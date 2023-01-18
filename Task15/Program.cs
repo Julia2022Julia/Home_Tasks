3f1344078a0d3cbc0fbe27ff6fb1f41925f7d29d
Julia <ulia.0496@gmail.com>
2023-01-18 19:28:42 +0300

    done task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа принимает цифру дня недели и проверяет, является ли выходным
+
+Console.Clear();
+Console.Write("Введите цифру, обозначающую день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 6 || n == 7)
+   Console.WriteLine("Yes");
+else 
+   Console.WriteLine("No");
