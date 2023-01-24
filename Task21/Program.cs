80a3d63a265beadac2c3f96b0935bb39a4b4fa3c
Julia <ulia.0496@gmail.com>
2023-01-24 10:10:37 +0300

    comment

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -15,3 +15,22 @@ Console.Write("Введите Z2: ");
 double z2 = Convert.ToDouble(Console.ReadLine());
 double n = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
 Console.WriteLine(n);
+
+
+// Task 21
+
+// Console.Clear();
+// Console.Write("Введите координату X: ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите координату Z: ");
+// double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите координату X: ");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите координату Y: ");
+// double y2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите координату Z: ");
+// double z2 = Convert.ToDouble(Console.ReadLine());
+// double S = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+// Console.WriteLine($"Результат: {Math.Round(S, 2)}");
