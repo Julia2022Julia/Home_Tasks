190b2f519bac0fc4b2d887705e1ed41b545215e7
Julia <ulia.0496@gmail.com>
2023-01-26 21:56:48 +0300

    done task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран(ввод пользователя с клавиатуры).
+
+Console.Clear();
+Console.WriteLine("Введите 8 чисел:");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
